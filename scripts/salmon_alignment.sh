5c310db3ab3e78130644a08a9cbc475db44b98be
fgaudilliere <flora.gaudilliere@ens-lyon.fr>
2020-12-16 09:07:56 +0000

    changement au script salmon pour ne pas écraser ce qui est déjà fait dans la boucle

--- scripts/salmon_alignment.sh
+++ scripts/salmon_alignment.sh
@@ -2,14 +2,14 @@ data=/ifb/data/mydatalocal/data_tp_ngs
 
 # Creating a salmon index
 
-salmon index -t $data/trinity_results/Trinity_RF.fasta -i $data/salmon_index -p 4
+#salmon index -t $data/trinity_results/Trinity_RF.fasta -i $data/salmon_index -p 4
 
 # Alignment on created index
 
-for n in 1 2 3 4 5 6
-do
-salmon quant -i $data/salmon_index -l A -1  $data/trimmomatic_outputs/Lib${n}_output_forward_paired.fq.gz -2 $data/trimmomatic_outputs/Lib${n}_output_reverse_paired.fq.gz --validateMappings -o $data/salmon_alignment
-done
+#for n in 1 2 3 4 5 6
+#do
+#salmon quant -i $data/salmon_index -l A -1  $data/trimmomatic_outputs/Lib${n}_output_forward_paired.fq.gz -2 $data/trimmomatic_outputs/Lib${n}_output_reverse_paired.fq.gz --validateMappings --gcBias -p 4 -o $data/salmon_alignment
+#done
 
 # Bons résultats : > 80% de reads alignés
 # Ici: inserts trop petits, reads se chevauchent
@@ -20,6 +20,6 @@ done
 
 for n in 1 2 3 4 5 6
 do
-salmon quant -i $data/salmon_index -l A -r $data/trimmomatic_outputs/Lib${n}_output_forward_paired.fq.gz --validateMappings -o $data/salmon_alignment_single_end
-salmon quant -i $data/salmon_index -l A -r $data/trimmomatic_outputs/Lib${n}_output_reverse_paired.fq.gz --validateMappings -o $data/salmon_alignment_single_end
+salmon quant -i $data/salmon_index -l A -r $data/trimmomatic_outputs/Lib${n}_output_forward_paired.fq.gz --validateMappings --gcBias -p 4 -o $data/salmon_alignment_single_end${n}_forward
+salmon quant -i $data/salmon_index -l A -r $data/trimmomatic_outputs/Lib${n}_output_reverse_paired.fq.gz --validateMappings --gcBias -p 4 -o $data/salmon_alignment_single_end${n}_reverse
 done
